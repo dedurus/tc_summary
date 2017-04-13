50b9b17a16ba52dafef3bf16d6ab50cd5af2beab
Dejan Duracoski <dedurac@gmail.com>
2017-04-13 21:42:43 +0200

    Export btn added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1038,7 +1038,15 @@
     // ******* ./ SUMMARY REPORTS ******* //
 
 
-
+    // export btn
+   /* $('.material-button-toggle').on("click", function () {
+        $(this).toggleClass('open');
+        $('.option').toggleClass('scale-on');
+    });*/
+    $('.material-button').tooltip({
+        'title': 'Export Report',
+        'container': 'body'
+    });
 
 
 
